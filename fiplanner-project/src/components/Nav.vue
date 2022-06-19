4ebe955ce5b7b8fd1720cc234c222ad2f1ca4a2d
Jason Hartanto <jason.hartanto@binus.ac.id>
2022-06-19 10:37:48 +0700

    update

--- fiplanner-project/src/components/Nav.vue
+++ fiplanner-project/src/components/Nav.vue
@@ -2,22 +2,38 @@
   <nav class="text-white bg-black p-4 flex justify-between items-center">
     <!-- nav-logo -->
     <div class="w-2/5 p-4">
-      <h1 class="font-bold text-2xl">Fi<span class="text-primary">Planner</span></h1>
+      <h1 class="font-bold text-2xl">
+        Fi<span class="text-primary">Planner</span>
+      </h1>
     </div>
     <!-- end nav logo -->
 
     <!-- nav-link -->
-    <ul class="list-none flex justify-evenly w-3/5 p-4 text-lg font-semibold">
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/forum">Forum</router-link></li>
-      <li><router-link to="/discover">Discover</router-link></li>
-      <li><router-link to="/blog">Blog</router-link></li>
+    <ul
+      id="list"
+      class="list-none flex justify-evenly w-3/5 p-4 text-lg font-semibold"
+    >
+      <li class="home">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li class="forum">
+        <router-link to="/forum">Forum</router-link>
+      </li>
+      <li class="discover">
+        <router-link to="/discover">Discover</router-link>
+      </li>
+      <li class="blog"><router-link to="/blog">Blog</router-link></li>
     </ul>
     <!-- end nav-link -->
 
     <!-- sign-in button -->
     <div class="w-2/5 flex justify-end">
-      <button @click="$router.push('signin')" class="hover:bg-green-800 duration-300 text-white py-3 mr-8 px-8 font-semibold bg-primary rounded text-l">Sign In</button>
+      <button
+        @click="$router.push('signin')"
+        class="hover:bg-green-800 duration-300 text-white py-3 mr-8 px-8 font-semibold bg-primary rounded text-l"
+      >
+        Sign In
+      </button>
     </div>
     <!-- end sign-in button -->
   </nav>
@@ -25,6 +41,27 @@
 
 <script>
 export default {};
+document.addEventListener("DOMContentLoaded", function (event) {
+  function navbarColor() {
+    document.querySelector("ul li").style.color = "white";
+    let currLoc = window.location.pathname;
+    console.log("test");
+    if (currLoc == "/") {
+      document.getElementsByClassName("home")[0].style.color = "#1AA37A";
+    } else if (currLoc == "/forum") {
+      document.getElementsByClassName("forum")[0].style.color = "#1AA37A";
+    } else if (currLoc == "/discover") {
+      document.getElementsByClassName("discover")[0].style.color = "#1AA37A";
+    } else if (currLoc == "/blog") {
+      document.getElementsByClassName("blog")[0].style.color = "#1AA37A";
+    }
+  }
+
+  navbarColor();
+  // document.querySelectorAll("li").addEventListener("click", function () {
+  //   navbarColor();
+  // });
+});
 </script>
 
 <style></style>
--- fiplanner-project/src/views/HomeView.vue
+++ fiplanner-project/src/views/HomeView.vue
@@ -8,13 +8,13 @@
 
 <script>
 // @ is an alias to /src
-import Home from '@/components/Home/Home.vue';
-import About from '@/components/Home/About.vue';
-import Benefits from '@/components/Home/Benefits.vue';
-import Consult from '@/components/Home/Consult.vue';
-import Footer from '@/components/Footer.vue';
+import Home from "@/components/Home/Home.vue";
+import About from "@/components/Home/About.vue";
+import Benefits from "@/components/Home/Benefits.vue";
+import Consult from "@/components/Home/Consult.vue";
+import Footer from "@/components/Footer.vue";
 export default {
-  name: 'HomeView',
-  components: { Home, About, Benefits, Consult, Footer },
+  name: "HomeView",
+  components: {Home, About, Benefits, Consult, Footer},
 };
 </script>
